842f06b67d4e20d8689acd75031dce3a66fee096
Ben Gohlke <jcgohlke@me.com>
2017-07-26 11:25:25 -0400

    Can show form and collect data from user and return it in a simple page.

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 var app = express();
 
+// Configure mustache with Express
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+// Allows public folder to be served statically to browsers
+app.use(express.static('public'));
+
+// Configure Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(request, response) {
-  response.send('Hello');
+  response.render('form');
+});
+
+app.post('/', function(request, response) {
+  response.render('answers', { answers: request.body });
 });
 
 app.listen(3000, function() {
