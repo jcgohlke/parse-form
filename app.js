c9544da6c10846cd272d464b5e7656b7983bc692
Ben Gohlke <jcgohlke@me.com>
2017-07-26 13:37:56 -0400

    Finished adding support for form data validation.

--- app.js
+++ app.js
@@ -17,12 +17,63 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Configure Express Validator
+app.use(expressValidator());
+
 app.get('/', function(request, response) {
   response.render('form');
 });
 
 app.post('/', function(request, response) {
-  response.render('answers', { answers: request.body });
+  var schema = {
+    'name': {
+      notEmpty: true,
+      isLength: {
+        options: [{ max: 100 }],
+        errorMessage: 'Name must not be longer than 100 characters'
+      },
+      errorMessage: 'Invalid Name'
+    },
+    'email': {
+      notEmpty: true,
+      isEmail: {
+        errorMessage: 'Invalid Email'
+      },
+      isLength: {
+        options: [{ max: 100 }],
+        errorMessage: 'Email must not be longer than 100 characters'
+      }
+    },
+    'birthyear': {
+      optional: true,
+      isInt: {
+        options: { gt: 1899, lt: 2018 }
+      },
+      errorMessage: 'Invalid Date'
+    },
+    'position': {
+      notEmpty: true,
+      matches: {
+        options: [/\b(?:manager|developer|ui-designer|graphic-designer)\b/]
+      },
+      errorMessage: 'Invalid Position'
+    },
+    'password': {
+      notEmpty: true,
+      isLength: {
+        options: [{ min: 8 }]
+      },
+      errorMessage: 'Invalid Password'
+    }
+  };
+  request.assert(schema);
+  request.getValidationResult().then(function(results) {
+    if (results.isEmpty()) {
+      response.render('answers', { answers: request.body });
+    } else {
+      response.render('form', { errors: results.array() });
+    }
+  });
 });
 
 app.listen(3000, function() {
